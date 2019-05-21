326c0f735a8a7b9b5ba45f70a369cfdda2b79ce3
Erik Fastermann <erik@fastermann.de>
2019-05-21 20:15:08 +0200

    Reformated Go code with goimports

--- elo.go
+++ elo.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-    "net/http"
-    "time"
-    "log"
-    "net/url"
-    "fmt"
-    "github.com/PuerkitoBio/goquery"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func elo() {
-    client := &http.Client{
-        Timeout: 10 * time.Second,
-    }
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
 
-    for range time.NewTicker(time.Hour).C {
-        accs, err := allAccounts()
-        if err != nil {
-            log.Println("WEB-PARSER: ERROR reading from database.", err)
-            return
-        }
-        for _, acc := range accs {
-            url, err := url.Parse(fmt.Sprintf("https://www.leagueofgraphs.com/en/summoner/%s/%s", acc.Region, acc.Ign))
-            if err != nil {
-                log.Println("WEB-PARSER: ERROR escaping", url, err)
-                continue
-            }
-            link := url.String()
+	for range time.NewTicker(time.Hour).C {
+		accs, err := allAccounts()
+		if err != nil {
+			log.Println("WEB-PARSER: ERROR reading from database.", err)
+			return
+		}
+		for _, acc := range accs {
+			url, err := url.Parse(fmt.Sprintf("https://www.leagueofgraphs.com/en/summoner/%s/%s", acc.Region, acc.Ign))
+			if err != nil {
+				log.Println("WEB-PARSER: ERROR escaping", url, err)
+				continue
+			}
+			link := url.String()
 
-            res, err := client.Get(link)
-            if err != nil {
-                log.Println("WEB-PARSER: ERROR opening", link, err)
-                continue
-            }
-            defer res.Body.Close()
+			res, err := client.Get(link)
+			if err != nil {
+				log.Println("WEB-PARSER: ERROR opening", link, err)
+				continue
+			}
+			defer res.Body.Close()
 
-            doc, err := goquery.NewDocumentFromReader(res.Body)
-            if err != nil {
-                log.Println("WEB-PARSER: ERROR parsing", link, err)
-                continue
-            }
-            leagueTier := doc.Find(".leagueTier").Text()
-            if leagueTier == "" {
-                log.Println("WEB-PARSER: ERROR finding .leagueTier", link)
-                continue
-            }
+			doc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Println("WEB-PARSER: ERROR parsing", link, err)
+				continue
+			}
+			leagueTier := doc.Find(".leagueTier").Text()
+			if leagueTier == "" {
+				log.Println("WEB-PARSER: ERROR finding .leagueTier", link)
+				continue
+			}
 
-            tokenPrep, err := db.Prepare("UPDATE accounts SET Elo=? WHERE ID=?")
-            if err != nil {
-                log.Println("WEB-PARSER: FAILED preparing Elo", leagueTier, "for Account", acc.Ign, err)
-                continue
-            }
-            _, err = tokenPrep.Exec(leagueTier, acc.ID)
-            if err != nil {
-                log.Println("WEB-PARSER: FAILED storing Elo", leagueTier, "for Account", acc.Ign, err)
-                continue
-            }
-            log.Println("WEB-PARSER: SUCCESS storing Elo:", leagueTier, "for Account", acc.Ign)
-        }
-    }
+			tokenPrep, err := db.Prepare("UPDATE accounts SET Elo=? WHERE ID=?")
+			if err != nil {
+				log.Println("WEB-PARSER: FAILED preparing Elo", leagueTier, "for Account", acc.Ign, err)
+				continue
+			}
+			_, err = tokenPrep.Exec(leagueTier, acc.ID)
+			if err != nil {
+				log.Println("WEB-PARSER: FAILED storing Elo", leagueTier, "for Account", acc.Ign, err)
+				continue
+			}
+			log.Println("WEB-PARSER: SUCCESS storing Elo:", leagueTier, "for Account", acc.Ign)
+		}
+	}
 }
-
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "log"
-    "net/http"
-    "html/template"
-    "github.com/gorilla/mux"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 var templates *template.Template
@@ -16,42 +17,41 @@ var templates *template.Template
 var db *sql.DB
 
 func main() {
-    envVar := func(env string) (string) {
-        str := os.Getenv(env)
-        if str == "" {
-            log.Fatalln("Environment variable", env, "is empty or doesn't exist")
-        }
-        return str
-    }
+	envVar := func(env string) string {
+		str := os.Getenv(env)
+		if str == "" {
+			log.Fatalln("Environment variable", env, "is empty or doesn't exist")
+		}
+		return str
+	}
 
-    templateDir := envVar("LEAGUE_ACCS_TEMPLATE_DIR")
-    templates = template.Must(template.ParseGlob(templateDir))
+	templateDir := envVar("LEAGUE_ACCS_TEMPLATE_DIR")
+	templates = template.Must(template.ParseGlob(templateDir))
 
-    dbUser := envVar("LEAGUE_ACCS_DB_USER")
-    dbPassword := envVar("LEAGUE_ACCS_DB_PASSWORD")
-    dbAddress := envVar("LEAGUE_ACCS_DB_ADDRESS")
-    dbName := envVar("LEAGUE_ACCS_DB_NAME")
-    var err error
-    db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbAddress, dbName))
-    if err != nil {
-        log.Fatal(err)
-    }
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	dbUser := envVar("LEAGUE_ACCS_DB_USER")
+	dbPassword := envVar("LEAGUE_ACCS_DB_PASSWORD")
+	dbAddress := envVar("LEAGUE_ACCS_DB_ADDRESS")
+	dbName := envVar("LEAGUE_ACCS_DB_NAME")
+	var err error
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbAddress, dbName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    go elo()
-    router := mux.NewRouter()
-    router.HandleFunc("/login", loginGet).Methods("GET")
-    router.HandleFunc("/login", loginPost).Methods("POST")
-    router.HandleFunc("/", accounts).Methods("GET")
-    router.HandleFunc("/edit/{id:[0-9]+}", editGet).Methods("GET")
-    router.HandleFunc("/edit/{id:[0-9]+}", editPost).Methods("POST")
-    router.HandleFunc("/new", createGet).Methods("GET")
-    router.HandleFunc("/new", createPost).Methods("POST")
-    log.Println("Starting server on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", router))
+	go elo()
+	router := mux.NewRouter()
+	router.HandleFunc("/login", loginGet).Methods("GET")
+	router.HandleFunc("/login", loginPost).Methods("POST")
+	router.HandleFunc("/", accounts).Methods("GET")
+	router.HandleFunc("/edit/{id:[0-9]+}", editGet).Methods("GET")
+	router.HandleFunc("/edit/{id:[0-9]+}", editPost).Methods("POST")
+	router.HandleFunc("/new", createGet).Methods("GET")
+	router.HandleFunc("/new", createPost).Methods("POST")
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
