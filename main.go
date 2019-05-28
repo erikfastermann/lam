5e401f99b8d5806613c2e1fff8eadbd530332632
Erik Fastermann <erik@fastermann.de>
2019-05-28 18:42:29 +0200

    Updated main to use new packages

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"github.com/erikfastermann/league-accounts/db"
+	"github.com/erikfastermann/league-accounts/elo"
+	"github.com/erikfastermann/league-accounts/handler"
 )
-
-var templates *template.Template
-
-var db *sql.DB
 
 func main() {
 	envVar := func(env string) string {
@@ -25,33 +20,20 @@ func main() {
 		return str
 	}
 
+	user := envVar("LEAGUE_ACCS_DB_USER")
+	password := envVar("LEAGUE_ACCS_DB_PASSWORD")
+	address := envVar("LEAGUE_ACCS_DB_ADDRESS")
+	name := envVar("LEAGUE_ACCS_DB_NAME")
+	db, err := db.New(user, password, address, name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.DB.Close()
+
 	templateDir := envVar("LEAGUE_ACCS_TEMPLATE_DIR")
-	templates = template.Must(template.ParseGlob(templateDir))
+	templates := template.Must(template.ParseGlob(templateDir))
 
-	dbUser := envVar("LEAGUE_ACCS_DB_USER")
-	dbPassword := envVar("LEAGUE_ACCS_DB_PASSWORD")
-	dbAddress := envVar("LEAGUE_ACCS_DB_ADDRESS")
-	dbName := envVar("LEAGUE_ACCS_DB_NAME")
-	var err error
-	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbAddress, dbName))
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	go elo()
-	router := mux.NewRouter()
-	router.HandleFunc("/login", loginGet).Methods("GET")
-	router.HandleFunc("/login", loginPost).Methods("POST")
-	router.HandleFunc("/", accounts).Methods("GET")
-	router.HandleFunc("/edit/{id:[0-9]+}", editGet).Methods("GET")
-	router.HandleFunc("/edit/{id:[0-9]+}", editPost).Methods("POST")
-	router.HandleFunc("/new", createGet).Methods("GET")
-	router.HandleFunc("/new", createPost).Methods("POST")
-	log.Println("Starting server on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	go elo.Parse(db)
+	h := handler.New(db, templates)
+	log.Fatal(http.ListenAndServe(":8080", h))
 }
